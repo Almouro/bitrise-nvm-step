95b68a07873b9f5777dd48275e5e0492f2d9d66d
Almouro <contact@almouro.com>
2016-07-24 19:02:38 +0200

    Change NVM version specification

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 
 PROFILE=$HOME/.profile
 touch $PROFILE
-curl -o- https://raw.githubusercontent.com/creationix/nvm/${nvm_version}/install.sh | PROFILE=$PROFILE bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v${nvm_version}/install.sh | PROFILE=$PROFILE bash
 source $PROFILE
 nvm install ${node_version}
 
