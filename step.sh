86279eec5b8950d20336cb30db64cae20dff12b3
sturman <iskoroman@gmail.com>
2020-05-09 12:41:23 +0300

    Use organization github links in docs and for downloading install.sh script

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@
 # Install NVM
 PROFILE=$HOME/.profile
 touch $PROFILE
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v${nvm_version}/install.sh | PROFILE=$PROFILE bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${nvm_version}/install.sh | PROFILE=$PROFILE bash
 source $PROFILE
 
 if [ ! -z "${working_dir}" ] ; then
